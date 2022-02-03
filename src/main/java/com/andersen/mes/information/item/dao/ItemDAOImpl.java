856cc42e3467985357fbc09686ae51354eb8c1c4
jw980506 <jw980506@naver.com>
2022-02-03 13:32:57 +0900

    ItemDAO 수정

--- src/main/java/com/andersen/mes/information/item/dao/ItemDAOImpl.java
+++ src/main/java/com/andersen/mes/information/item/dao/ItemDAOImpl.java
@@ -2,16 +2,23 @@ package com.andersen.mes.information.item.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 
 import com.andersen.mes.information.item.vo.ItemVO;
 
 public class ItemDAOImpl implements ItemDAO {
 
+	@Autowired
+	private SqlSession sqlSession;
+	
 	@Override
 	public List selectAllItemList() throws DataAccessException {
 		
-		return null;
+		List<ItemVO> listAllItem = null;
+		listAllItem = sqlSession.selectList("mapper.information.employee.selectAllItemList");
+		return listAllItem;
 	}
 
 	@Override
